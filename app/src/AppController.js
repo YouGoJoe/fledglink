3a6e2a29aefa1fac10e79820d23d5e6e934f4427
Unknown <joestaller23@gmail.com>
2016-12-24 13:19:05 -0500

    Final look done for Xmas launch

--- app/src/AppController.js
+++ app/src/AppController.js
@@ -4,7 +4,7 @@
  * @param $mdSidenav
  * @constructor
  */
-function AppController() {
+function AppController($http) {
   var vm = this;
   var DEFAULT_LIMIT = 65;
 
@@ -19,100 +19,6 @@ function AppController() {
       (window.matchMedia && window.matchMedia("(-webkit-device-pixel-ratio: 0.5),(-moz-device-pixel-ratio: 0.5),(device-pixel-ratio: 0.5)").matches)
     )
   };
-
-  vm.podcasts = [
-    {
-      id: 1,
-      title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-      hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-      Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-      tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-      source: './podcasts/Ep1 - Productivity _RAW.mp3',
-    },
-    {
-      id: 2,
-      title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-      hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-      Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-      tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-      source: './podcasts/Ep2 - Gated Content - Compressed.mp3',
-    },
-    {
-      id: 3,
-      title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-      hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-      Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-      tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-      source: '',
-    },
-    // {
-    //   id: 9,
-    //   title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-    //   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-    //   hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-    //   Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-    //   tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-    //   source: '',
-    // },
-    // {
-    //   id: 10,
-    //   title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-    //   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-    //   hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-    //   Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-    //   tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-    //   source: '',
-    // },
-    // {
-    //   id: 11,
-    //   title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-    //   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-    //   hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-    //   Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-    //   tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-    //   source: '',
-    // },
-    // {
-    //   id: 12,
-    //   title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-    //   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-    //   hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-    //   Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-    //   tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-    //   source: '',
-    // },
-    // {
-    //   id: 13,
-    //   title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-    //   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-    //   hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-    //   Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-    //   tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-    //   source: '',
-    // },
-    // {
-    //   id: 14,
-    //   title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-    //   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-    //   hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-    //   Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-    //   tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-    //   source: '',
-    // },
-    // {
-    //   id: 15,
-    //   title: 'Punny Podcast Title, Padded For Length So That We Can See How A Long Title Looks',
-    //   description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultrices erat nec erat condimentum, 
-    //   hendrerit feugiat nibh mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-    //   Cras volutpat, ligula et viverra convallis, magna ligula molestie est, in vestibulum nunc ex nec justo. Donec erat libero, 
-    //   tincidunt vitae orci non, dignissim pretium sem. Nunc sit amet faucibus odio. Sed`,
-    //   source: '',
-    // },
-
-  ]
 
   vm.select = function (podcast) {
     vm.podcasts.forEach(function (cast) {
@@ -154,11 +60,14 @@ function AppController() {
     vm.moreClicked = false;
   }
 
-  vm.init = function () {
-    vm.select(vm.podcasts[0]);
+  vm.init = function () {    
+    $http.get('src/podcasts.json').success(function(podcasts){
+      vm.podcasts = podcasts;
+      vm.select(vm.podcasts[0]);
+    })
   } ();
 
 
 }
 
-export default [AppController];
+export default ['$http', AppController];
